55836e86f540925e8add3bf684d8ea0bf6d64b39
Tohi Kim <tohikimbusiness@gmail.com>
2026-04-18 17:45:19 +0200

    Added components

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import Header from './Header.jsx';
+import Body from './Body.jsx';
+import Footer from './Footer.jsx';
+
+function Home(){
+    return (
+        <>
+        <Header />
+        <Body />
+        <Footer />
+        </>
+    )
+}
+
+export default Home;
